567e6c1c5ceaa253cb4c0910f63b2b0584da1445
Zahid <zahid.hzade001@gmail.com>
2024-08-22 19:07:35 +0400

    errorPage

--- src/router/Route.jsx
+++ src/router/Route.jsx
@@ -2,12 +2,14 @@ import { createBrowserRouter, createRoutesFromElements, Route } from "react-rout
 import HomeLayout from "../layout/HomeLayout";
 import Home from "../pages/Home";
 import Projects from "../pages/Projects";
+import ErrorPage from "../components/ErrorPage";
 
 export const route = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<HomeLayout />}>
             <Route path="/" element={<Home />} />
             <Route path="/projects" element={<Projects />} />
+            <Route path="*" element={<ErrorPage />} />
         </Route>
     )
 )
